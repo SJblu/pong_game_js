e9f44f8fb5effae84b7612e9273327efe5e38a78
SILVIO SOUZA <sjblu7@gmail.com>
2021-01-21 22:24:49 -0400

    Implementado ator e movimentacao para Raquete Esquerdo; Implementado biblioteca p5.collide2d.js para colisao entre atores bolinha e raquete

--- pong.js
+++ pong.js
@@ -1,12 +1,20 @@
-//Dimensão e local de partida da bolinha
+//Dimensão e coordenadas de partida da bolinha
 let xBola = 300
 let yBola = 200
 let bolaDiametro = 20
 let raio = bolaDiametro / 2
 
+//dimensao e coordenadas de partida da raquete
+let xRaqueteEsquerda = 20
+let yRaqueteEsquerda = 150
+let larguraRaquete = 20
+let alturaRaquete = 100
+
 //velocidade de movimento da bolinha
 let velocidadeXBola = 4
 let velocidadeYBola = 4
+
+let colisao = false
 
 //Dimensao do cenario
 function setup() {
@@ -15,10 +23,14 @@ function setup() {
 
 //Funcao draw é nativa do p5.js. Neste caso exibe a cor do cenario e chama as funções para movimentação dos atores
 function draw() {
-  background(0, 0, 0);
+  background(0, 0, 0)
   desenhaBola()
+  desenhaRaqueteEsquerda()
   movimentoBola()
   colisaoBordaBola()
+  movimentoRaqueteEsquerda()
+  //colisaoBolaRaqueteEsquerda()
+  colisaoBolaRaqueteEsquerdaCollideLib()
   
 }
   
@@ -26,8 +38,12 @@ function desenhaBola(){
   circle(xBola, yBola, bolaDiametro)
 }
 
+function desenhaRaqueteEsquerda(){
+  rect(xRaqueteEsquerda, yRaqueteEsquerda, larguraRaquete, alturaRaquete)
+}
+
 function movimentoBola(){
-  xBola += velocidadeXBola
+  xBola -= velocidadeXBola
   yBola += velocidadeYBola
   
 }
@@ -38,5 +54,28 @@ function colisaoBordaBola() {
    }
   if (yBola+raio > height || yBola-raio < 0) {
     velocidadeYBola *= -1
+  }
+}
+
+function movimentoRaqueteEsquerda(){
+  if (keyIsDown(UP_ARROW) && yRaqueteEsquerda > 0) {
+    yRaqueteEsquerda -= 10
+  }
+  if (keyIsDown(DOWN_ARROW) && yRaqueteEsquerda < (height-alturaRaquete)) {
+    yRaqueteEsquerda += 10
+  }
+}
+
+//funcao de colisao substituida ao implementar biblioteca Collide2D
+function colisaoBolaRaqueteEsquerda(){
+  if (xBola-raio < (xRaqueteEsquerda + larguraRaquete) && yBola-raio < (yRaqueteEsquerda + alturaRaquete) && yBola+raio > yRaqueteEsquerda) {
+    velocidadeXBola *= -1
+   } 
+}
+
+function colisaoBolaRaqueteEsquerdaCollideLib(){
+  colisao = collideRectCircle(xRaqueteEsquerda, yRaqueteEsquerda, larguraRaquete, alturaRaquete, xBola, yBola, raio);
+  if(colisao){
+    velocidadeXBola *= -1
   }
 }
